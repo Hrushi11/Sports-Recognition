3ffe99e20a269b574408b48030e440c4ee8584df
Hrushikesh Kachgunde <73577192+Hrushi11@users.noreply.github.com>
2021-10-06 12:52:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function run() {
   const MODEL_URL =
-    "https://raw.githubusercontent.com/Hrushi11/Sports-Recognition/main/JS-Model/model.json";
+    "https://raw.githubusercontent.com/Hrushi11/Sports-Recognition/main/Seq-JS/model.json";
   const model = await tf.loadLayersModel(MODEL_URL);
   console.log(model.summary());
   //   const input = tf.tensor2d([10.0], [1, 1]);
